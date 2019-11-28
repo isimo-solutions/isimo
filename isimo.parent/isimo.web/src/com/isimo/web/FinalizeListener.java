a68e95e7e0073cc904b4c76f2425ad5ac5c3495c
ArturWenda <art.wen@o2.pl>
2019-11-28 13:43:45 +0100

    Firefox process is now terminated after test execution

--- isimo.parent/isimo.web/src/com/isimo/web/FinalizeListener.java
+++ isimo.parent/isimo.web/src/com/isimo/web/FinalizeListener.java
@@ -3,11 +3,16 @@ package com.isimo.web;
 import org.springframework.stereotype.Component;
 
 import com.isimo.core.event.Event;
+import com.isimo.core.event.EventType;
 import com.isimo.core.event.ExecutionListener;
 
 @Component
 public class FinalizeListener implements ExecutionListener<Event> {
 	@Override
 	public void handleEvent(Event event) {
+		if(event.getEventType() == EventType.StopTestCase) {
+			System.out.println("Quitting web driver!");
+			WebDriverProvider.getInstance().getWebDriver().quit();
+		}
 	}
 }
