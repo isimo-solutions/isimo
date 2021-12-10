ea789e274c3b177c9a3edeb6cb825593938f43ec
Andrzej Dmoch <95031588@mail.schwarz>
2021-12-10 10:15:47 +0100

    Settings path extracted

--- isimo.parent/isimo-build-resources-local.cmd
+++ isimo.parent/isimo-build-resources-local.cmd
@@ -1,2 +1,3 @@
 set RESOURCESURL=file:c:/isimoresources
-mvn -s f:\git\settings.xml -Dgecko.driver.url=%RESOURCESURL% -Dfirefox.download.url=%RESOURCESURL% -Dchrome.driver.url=%RESOURCESURL%/chromedriver_win32.zip -Die.driver.download.url=%RESOURCESURL% -Dedge.driver.url=%RESOURCESURL%/edgedriver_win32.zip clean install
+set SETTINGSFILE=f:\git\settings.xml
+mvn -s %SETTINGSFILE% -Dgecko.driver.url=%RESOURCESURL% -Dfirefox.download.url=%RESOURCESURL% -Dchrome.driver.url=%RESOURCESURL%/chromedriver_win32.zip -Die.driver.download.url=%RESOURCESURL% -Dedge.driver.url=%RESOURCESURL%/edgedriver_win32.zip clean install
